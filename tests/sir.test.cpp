8c6f492334197fbc93c4ccdc1e0df3a077a2d622
Matteo Bonacini <matteo.bonna@gmail.com>
2021-05-10 22:33:33 +0200

    Add tests

--- tests/sir.test.cpp
+++ tests/sir.test.cpp
@@ -6,6 +6,21 @@
 #include "doctest.h"
 #include "model.hpp"
 
+// These are not const because it is required by a test.
+struct TestData {
+  double b{};
+  double c{};
+  int s{};
+  int i{};
+  int r{};
+};
+
+std::ostream& operator<< (std::ostream& os, const TestData& value) {
+  os << "b: " << value.b << ", c: " << value.c << ", s: " << value.s
+     << ", i: " << value.i << ", r: " << value.r;
+  return os;
+}
+
 TEST_CASE("SIR Model constructor parameters checks") {
   // todo replace this with smart pointers once they are taught in lesson
   // since no delete is called (for now)
@@ -14,41 +29,31 @@ TEST_CASE("SIR Model constructor parameters checks") {
   // We use require in the first two subcases, since the rest of
   // the implementation strictly relies on these assertions.
   SUBCASE("Valori corretti 1") {
-    const double b = 0;
-    const double c = 1;
+    TestData td{0, 1, 1, 2, 3};
 
-    const int s = 1;
-    const int i = 2;
-    const int r = 3;
+    REQUIRE_NOTHROW(model = new sir::Model{td.b, td.c, td.s, td.i, td.r});
 
-    REQUIRE_NOTHROW(model = new sir::Model{b, c, s, i, r});
+    REQUIRE_EQ(model->beta(), td.b);
+    REQUIRE_EQ(model->gamma(), td.c);
+    REQUIRE_EQ(model->susceptible(), td.s);
+    REQUIRE_EQ(model->infected(), td.i);
+    REQUIRE_EQ(model->removed(), td.r);
 
-    REQUIRE_EQ(model->beta(), b);
-    REQUIRE_EQ(model->gamma(), c);
-    REQUIRE_EQ(model->susceptible(), s);
-    REQUIRE_EQ(model->infected(), i);
-    REQUIRE_EQ(model->removed(), r);
-
-    REQUIRE_EQ(model->total(), s + i + r);
+    REQUIRE_EQ(model->total(), td.s + td.i + td.r);
   }
 
   SUBCASE("Valori corretti 2") {
-    const double b = 0.2;
-    const double c = 0.8;
+    TestData td{0.2, 0.8, 5, 9, 14};
 
-    const int s = 5;
-    const int i = 9;
-    const int r = 14;
+    REQUIRE_NOTHROW(model = new sir::Model{td.b, td.c, td.s, td.i, td.r});
 
-    REQUIRE_NOTHROW(model = new sir::Model{b, c, s, i, r});
+    REQUIRE_EQ(model->beta(), td.b);
+    REQUIRE_EQ(model->gamma(), td.c);
+    REQUIRE_EQ(model->susceptible(), td.s);
+    REQUIRE_EQ(model->infected(), td.i);
+    REQUIRE_EQ(model->removed(), td.r);
 
-    REQUIRE_EQ(model->beta(), b);
-    REQUIRE_EQ(model->gamma(), c);
-    REQUIRE_EQ(model->susceptible(), s);
-    REQUIRE_EQ(model->infected(), i);
-    REQUIRE_EQ(model->removed(), r);
-
-    REQUIRE_EQ(model->total(), s + i + r);
+    REQUIRE_EQ(model->total(), td.s + td.i + td.r);
   }
 
   SUBCASE("Valori incorretti") {
@@ -61,6 +66,8 @@ TEST_CASE("SIR Model constructor parameters checks") {
     CHECK_THROWS(model = new sir::Model{0.5, 1, -1, -2, 3});
 
     CHECK_THROWS(model = new sir::Model{0.5, 1, 0, 0, 0});
+
+    CHECK_THROWS(model = new sir::Model{0.5, 1, 0, 0, 5000000});
   }
 }
 
@@ -76,52 +83,60 @@ std::mt19937 generator{rd()};
 // [a, b]. This is irrelevant, since the chances of landing exactly on
 // b for double types is very small.
 std::uniform_real_distribution<double> betaGamma(0, 1);
-std::uniform_int_distribution<int> greaterThanZero(1, +2147483647);
-std::uniform_int_distribution<int> stepCount(1, 1000000);
-
-struct
+std::uniform_int_distribution<int> greaterThanZero(1, 5E6);
+std::uniform_int_distribution<int> stepCount(1, 1E6);
 
 TEST_CASE("SIR Model with random values") {
   // Repeat test multiple times with random values
-  for (int test = 0; test < 50; ++test) {
-    const double b = betaGamma(generator);
-    const double c = betaGamma(generator);
+  TestData td;
 
-    const int s = greaterThanZero(generator);
-    const int i = greaterThanZero(generator);
-    const int r = greaterThanZero(generator);
+  // fixme might be worth asking
+  // no real way to do cases with parametrized data.
+  SUBCASE("") {
+    td = {betaGamma(generator), betaGamma(generator),
+      greaterThanZero(generator), greaterThanZero(generator),
+      greaterThanZero(generator)};
+  }
+  SUBCASE("") {
+    td = {betaGamma(generator), betaGamma(generator),
+          greaterThanZero(generator), greaterThanZero(generator),
+          greaterThanZero(generator)};
+  }
+  SUBCASE("") {
+    td = {betaGamma(generator), betaGamma(generator),
+          greaterThanZero(generator), greaterThanZero(generator),
+          greaterThanZero(generator)};
+  }
+  SUBCASE("") {
+    td = {betaGamma(generator), betaGamma(generator),
+          greaterThanZero(generator), greaterThanZero(generator),
+          greaterThanZero(generator)};
+  }
+  SUBCASE("") {
+    td = {betaGamma(generator), betaGamma(generator),
+          greaterThanZero(generator), greaterThanZero(generator),
+          greaterThanZero(generator)};
+  }
 
-    std::stringstream subcaseName;
+  printf("%d %d %d\n", td.s, td.i, td.r);
+  REQUIRE(td.s + td.i + td.r > 0) ;
 
-    subcaseName << "Random test with values:" << b << ", " << c << ", "
-                << s << ", " << i << ", " << r << std::endl;
+  sir::Model model = sir::Model{td.b, td.c, td.s, td.i, td.r};
 
-    SUBCASE(subcaseName.str()) {
-      sir::Model model = sir::Model{b, c, s, i, r};
+  REQUIRE_EQ(model.total(), td.s + td.i + td.r);
 
-      // can't use greaterThanZero here, it's too large.
-      const int steps = stepCount(generator);
-      for (int step = 0; step < steps; ++step) {
-        model.step();
+  // can't use greaterThanZero here, it's too large.
+  const int steps = stepCount(generator);
+  for (int step = 0; step < steps; ++step) {
+    model.step();
 
-        CHECK_EQ(
-          model.susceptible() + model.infected() + model.removed(),
-          model.total());
+    CHECK_EQ(
+      model.susceptible() + model.infected() + model.removed(),
+      model.total());
 
-        CHECK(model.susceptible() >= 0);
-        CHECK(model.infected() >= 0);
-        CHECK(model.removed() >= 0);
-
-        // todo test, gamma inverse mean time of infection resolution
-        if (model.infected() <= 1) {
-          CHECK_EQ(
-            model.gamma(),
-            doctest::Approx(1.f / static_cast<float>(step)).epsilon(0.1));
-
-          break;
-        }
-      }
-    }
+    CHECK(model.susceptible() >= 0);
+    CHECK(model.infected() >= 0);
+    CHECK(model.removed() >= 0);
   }
 }
 
