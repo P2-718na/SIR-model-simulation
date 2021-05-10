b1e52b24c6e8d3c4f8e489907f56e010b0fc0283
Matteo Bonacini <matteo.bonna@gmail.com>
2021-05-10 14:09:17 +0200

    Add cmake binary output dir

--- tests/sir.test.cpp
+++ tests/sir.test.cpp
@@ -1,7 +1,7 @@
 // Doctest requires this define to be specified only once.
 // Probably this is the best place
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "../doctest.h"
+#include "doctest.h"
 
 TEST_CASE("SIR test") {
   CHECK(true);
