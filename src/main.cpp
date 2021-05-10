f7df4f996f1ea8b17e90ccb78c144370fcdaa82b
Matteo Bonacini <matteo.bonna@gmail.com>
2021-05-10 22:56:16 +0200

    Add error handling in main

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,31 @@
 #include "parser.hpp"
 
 int main(int argc, char* argv[]) {
-  // Parse CLI Arguments
-  sir::Parser parser(argc, argv);
+  //todo w8 for smart pointers
+  try {
+    // Parse CLI Arguments
+    sir::Parser parser{argc, argv};
 
-  // Initialise model
-  sir::Model model{parser.b(), parser.c(), parser.s(), parser.i(), parser.r()};
+    // Initialise model
+    sir::Model model{parser.b(), parser.c(), parser.s(), parser.i(), parser.r()};
 
-  // Initialise visualizer
-  sir::Visualizer visualizer{model};
+    // Initialise visualizer
+    sir::Visualizer visualizer{model};
 
-  // Visualize data
-  if (parser.prettyPrint()) {
-     visualizer.displayPretty(parser.dayCount());
-  } else {
-    visualizer.display(parser.dayCount(), parser.displayHeadings());
+    // Visualize data
+    if (parser.prettyPrint()) {
+      visualizer.displayPretty(parser.dayCount());
+    } else {
+      visualizer.display(parser.dayCount(), parser.displayHeadings());
+    }
+  } catch (const std::range_error &error) {
+    std::cerr << "Error: " << error.what() << std::endl;
+
+    return 1;
+  } catch (...) {
+    std::cerr << "Something went horribly wrong. Terminating." << std::endl;
+
+    return 1;
   }
 
   // Return
