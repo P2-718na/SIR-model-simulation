9ba2d4e26ee84076b8a43ad84a4e1f8fa60fe03f
Matteo Bonacini <matteo.bonna@gmail.com>
2021-05-10 22:33:01 +0200

    Add max value to model check

--- src/Model/model.cpp
+++ src/Model/model.cpp
@@ -9,26 +9,26 @@ Model::Model(
   double beta, double gamma, int susceptible, int infected, int removed) {
   // Check values for beta and gamma parameters.
   if (!this->beta(beta)) {
-    throw std::range_error("beta parameter is not in range.");
+    throw std::range_error("beta parameter out of range.");
   }
 
   if (!this->gamma(gamma)) {
-    throw std::range_error("gamma parameter is not in range.");
+    throw std::range_error("gamma parameter out of range.");
   }
 
   // Check values for s, i, r parameters.
-  if (susceptible < 0) {
-    throw std::range_error("susceptible parameter must be positive.");
+  if (susceptible < 0 || susceptible >= 5E6) {
+    throw std::range_error("susceptible parameter out of range.");
   }
   s_ = susceptible;
 
-  if (infected < 0) {
-    throw std::range_error("infected parameter must be positive.");
+  if (infected < 0 || infected >= 5E6) {
+    throw std::range_error("infected parameter out of range.");
   }
   i_ = infected;
 
-  if (removed < 0) {
-    throw std::range_error("removed parameter must be positive.");
+  if (removed < 0 || removed >= 5E6) {
+    throw std::range_error("removed parameter out of range.");
   }
   r_ = removed;
 
@@ -98,6 +98,7 @@ void Model::step() noexcept {
   // Since s + i + r is constant, update S based on that. This also accounts
   // for floating point and rounding errors.
   // S cannot be negative, since infected and removed are rounded down.
+  //fixme this does not work. fix. pls.
   s_ = n_ - this->infected() - this->removed();
 }
 }
